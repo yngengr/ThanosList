f6dc23675f64401a3f01506b3fec6c4871045049
young engineer <yngnier@gmail.com>
2023-06-17 16:52:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,21 +13,41 @@ const people = [
 /** 1번 문제: filter를 이용하여 500,000원 이상 돈을 가진 객체를 배열로 담기 */
 let rich = people.filter((person) => person.money >= 500_000);
 
-console.log("Rich People >>", rich); // [{name: '정훈', money: 500000}, {name: '선혁', money: 600000}]
+console.log("1. Rich People >>", rich); // [{name: '정훈', money: 500000}, {name: '선혁', money: 600000}]
 
 /** 2번 문제: reduce를 이용하여 500,000원 이상 돈을 가진 모든 "사람"의 이름만 요소로 배열에 담기 */
 let richNames = people
   .filter((person) => person.money >= 500_000)
   .map((person) => person.name);
 
-console.log(`Rich Names >> ${richNames}`);
+console.log(`2. Rich Names >> ${richNames}`);
 
 /** 3번 문제: reduce를 이용하여 최소값, 최대값 구하기 */
 const numbers = [1, 2, 3, 4, 5];
 
 let max = numbers.reduce((acc, ele) => (acc > ele ? acc : ele));
 let min = numbers.reduce((acc, ele) => (acc < ele ? acc : ele));
-console.debug(`배열 내의 최대값 : ${max}, 최소값 ${min}`);
+console.log(`3. 배열 내의 최대값 : ${max}, 최소값 ${min}`);
+
+/** 4번 문제 richNames reduce로 구현하기 */
+let richNames2 = people.reduce((acc, person, idx) => {
+  // console.log(`acc > ${acc}
+  // , person.name > ${person.name}
+  // , person.money > ${person.money}
+  // , person >> ${person}`);
+  // if (idx === people.length) {
+  //   return
+  // }
+
+  if (person.money >= 500000) {
+    return (acc += person.name + ",");
+  } else {
+    return acc;
+  }
+  // (person.money >= 500000 ? acc += person.name : acc += "");
+}, "");
+
+console.log(`4. reduce를 활용 >> ${richNames2.slice(0, -1)}`);
 
 /** To-do list 관련 함수 */
 const addButton = document.getElementById("add-button");
@@ -95,6 +115,6 @@ addButton.addEventListener("click", () => {
   text.value = ""; // 입력 이후에 input 태그를 비움
   text.focus(); // input 태그를 비우고 커서를 입력 칸으로 옮긴다.
 });
-ㅌ
+
 // ! 최초 실행부
 renderTodofromArray(todolist);
